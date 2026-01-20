068170e8746041646595609bb659b7e4117f38e8
kamalesh252906 <cherrykamalesh@gmail.com>
2026-01-20 09:21:07 +0530

    text changes

--- backend_api/routers/user_login.py
+++ backend_api/routers/user_login.py
@@ -13,7 +13,7 @@ def login_user(login: schemas.UserLogin, db: Session = Depends(get_db)):
         .filter(
             or_(
                 models.User.email == login.email,
-                models.User.phone == login.phone
+                models.User.phone == login.email
             )
         )
         .first()
